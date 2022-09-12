1998f34a3eb6d9c0f9659fd1fce7c83d53220d6c
vmarksabelita <mark.angelo@videris.us>
2022-09-12 17:35:30 +0800

    WIP

--- src/configs/laravel/repository.defaults.ts
+++ src/configs/laravel/repository.defaults.ts
@@ -7,27 +7,27 @@ const currentWorkingDirectory = process.cwd();
 export const defaultRepositoryFiles: RepositoryDefaultsInterface[] = [
     {
         name: "Controller",
-        sourceFile: `${projectPath}/module/laravel/controller/SampleWithBase.php`,
+        sourceFile: `${projectPath}/templates/module/laravel/controller/SampleWithBase.php`,
         destinationPath: `${currentWorkingDirectory}/app/Http/Controllers`,
     },
     {
         name: "Interface",
-        sourceFile: `${projectPath}/module/laravel/interface/SampleInterface.php`,
+        sourceFile: `${projectPath}/templates/module/laravel/interface/SampleInterface.php`,
         destinationPath: `${currentWorkingDirectory}/app/Interfaces`
     },
     {
         name: "Repository",
-        sourceFile: `${projectPath}/module/laravel/repository/SampleWithBase.php`,
+        sourceFile: `${projectPath}/templates/module/laravel/repository/SampleWithBase.php`,
         destinationPath: `${currentWorkingDirectory}/app/Repositories`
     },
     {
         name: "Provider",
-        sourceFile: `${projectPath}/module/laravel/provider/SampleProvider.php`,
+        sourceFile: `${projectPath}/templates/module/laravel/provider/SampleProvider.php`,
         destinationPath: `${currentWorkingDirectory}/app/Providers`
     },
     {
         name: "",
-        sourceFile: `${projectPath}/module/laravel/model/SampleModel.php`,
+        sourceFile: `${projectPath}/templates/module/laravel/model/SampleModel.php`,
         destinationPath: `${currentWorkingDirectory}/app/Models`
     },
 ]
